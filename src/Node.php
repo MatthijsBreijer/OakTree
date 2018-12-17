ecd48383373d7653fe641bef1d40a0a57736442c
MatthijsBreijer <45951102+MatthijsBreijer@users.noreply.github.com>
2018-12-17 22:46:32 +0100

    Add Node and NodeInterface

---
+++ src/Node.php
@@ -0,0 +1,216 @@
+<?php
+namespace MatthijsBreijer\OakTree;
+
+/*
+ * This file is part of OakTree.
+ * @author Matthijs Breijer
+ */
+class Node implements NodeInterface
+{
+	/**
+	 * @var null|NodeInterface
+	 */
+	private $parent = null;
+
+	/**
+	 * Child nodes
+	 * @var NodeInterface[]
+	 */
+	private $children = [];
+
+	/**
+	 * @var mixed
+	 */
+	private $value = null;
+
+    /**
+     * {@inheritdoc}
+     */
+	public function __construct($value = null, array $children = [])
+	{
+		$this->setValue($value);
+		$this->setChildren($children);
+	}
+
+    /**
+     * {@inheritdoc}
+     */
+	public function addChild(NodeInterface $child, $key = null) : NodeInterface
+	{
+		if (is_null($key)) {
+			$this->children[] = $child;
+		}
+		else {
+			$this->children[$key] = $child;
+		}
+		
+		$child->setParent($this);
+
+		return $this;
+	}
+
+    /**
+     * {@inheritdoc}
+     */
+	public function setParent(NodeInterface $parent = null) : NodeInterface
+	{
+		$this->parent = $parent;
+		return $this;
+	}
+
+    /**
+     * {@inheritdoc}
+     */
+	public function getChildren() : array
+	{
+		return $this->children;
+	}
+
+    /**
+     * {@inheritdoc}
+     */
+	public function getKeys() : array
+	{
+		return array_keys($this->children);
+	}
+
+    /**
+     * {@inheritdoc}
+     */
+	public function setChildren(array $children) : NodeInterface
+	{
+		foreach ($children as $key => $child) {
+			$this->addChild($child, $key);
+		}
+
+		return $this;
+	}
+
+    /**
+     * {@inheritdoc}
+     */
+	public function getParent()
+	{
+		return $this->parent;
+	}
+
+    /**
+     * {@inheritdoc}
+     */
+	public function setValue($value) : NodeInterface
+	{
+		$this->value = $value;
+		return $this;
+	}
+
+    /**
+     * {@inheritdoc}
+     */
+	public function getValue() 
+	{
+		return $this->value;
+	}
+
+	/**
+	 * {@inheritdoc}
+	 */
+	public function removeChild(NodeInterface $remove) : NodeInterface
+	{
+        foreach ($this->children as $key => $child) {
+            if ($child == $remove) {
+                unset($this->children[$key]);
+                $child->setParent(null);
+                break;
+            }
+        }
+
+        return $this;
+	}
+
+	/**
+	 * {@inheritdoc}
+	 */
+	public function isChild() : bool
+	{
+		return !$this->isRoot();
+	}
+
+	/**
+	 * {@inheritdoc}
+	 */
+	public function isRoot() : bool
+	{
+		return is_null($this->getParent());
+	}
+
+    /**
+     * {@inheritDoc}
+     */
+    public function isLeaf() : bool
+    {
+        return count($this->children) === 0;
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+	public function toArray($serializer = null) : array
+	{
+		$node = [
+			'value' => $this->processValue($this->getValue(), $serializer),
+			'children' => []
+		];
+
+		foreach ($this->children as $key => $childNode) {
+			$node['children'][$key] = $childNode->toArray($serializer);
+		}
+
+		return $node;
+	}
+
+	/**
+	 * (un)serialize value if Closure is passed
+	 * @param mixed $value
+	 * @param Closure|null $callback
+	 * @return mixed
+	 */
+	private function processValue($value, $closure = null)
+	{
+		if (is_callable($closure)) {
+			$value = $closure($value);
+		}
+
+		return $value;
+	}
+
+    /**
+     * {@inheritdoc}
+     */
+	public static function fromArray(array $array, $unserializer = null) : NodeInterface
+	{
+		$node = new Node(static::processValue($array['value'], $unserializer));
+
+		foreach ($array['children'] as $key => $child) {
+			$node->addChild(static::fromArray($child, $unserializer), $key);
+		}
+
+		return $node;
+	}
+
+    /**
+     * {@inheritdoc}
+     */
+	public function jsonSerialize() : array
+	{
+		return $this->toArray();
+	}
+
+    /**
+     * {@inheritdoc}
+     */
+    public function accept(Visitor $visitor)
+    {
+        return $visitor->visit($this);
+    }
+
+}
---
+++ src/NodeInterface.php
@@ -0,0 +1,110 @@
+<?php
+namespace MatthijsBreijer\OakTree;
+
+interface NodeInterface extends \JsonSerializable
+{
+
+	/**
+	 * @param mixed $value
+	 * @param NodeInterface[] $children 
+	 */
+	public function __construct($value, array $children = []);
+
+	/**
+	 * Add a child to node, automatically sets parent too
+	 * @param NodeInterface $child
+	 * @return NodeInterface self for fluency
+	 */
+    public function addChild(NodeInterface $child) : NodeInterface;
+
+    /**
+     * Set the parent of a node
+     * @param NodeInterface $parent
+     * @return NodeInterface for fluency
+     */
+	public function setParent(NodeInterface $parent = null) : NodeInterface;
+
+	/**
+	 * Get children of a node
+	 * @return NodeInterface[]
+	 */
+    public function getChildren() : array;
+
+    /**
+     * Get keys of the child nodes
+     * @return mixed[]
+     */
+    public function getKeys() : array;
+
+    /**
+     * Set the children of a Node, array keys are kept within the tree
+     * @param NodeInterface[] $children
+     * @return NodeInterface The Node children are added to for fluency
+     */
+	public function setChildren(array $children) : NodeInterface;
+
+    /**
+     * Get parent of a Node
+     * @return null|NodeInterface
+     */
+	public function getParent();
+
+    /**
+     * Set the value stored in the Node
+     * @return NodeInterface for fluency
+     */
+	public function setValue($value) : NodeInterface;
+
+    /**
+     * Get the value stored in the Node
+     * @return mixed
+     */
+	public function getValue();
+
+	/**
+	 * Remove child from tree
+	 * @param NodeInterface $child
+	 * @return NodeInterface for fluency
+	 */
+	public function removeChild(NodeInterface $child) : NodeInterface;
+
+	/**
+	 * Is Node a child of another node?
+	 * @return boolean
+	 */
+	public function isChild() : bool;
+
+	/**
+	 * Is Node the root of the tree (without a parent)?
+	 * @return boolean
+	 */
+	public function isRoot() : bool;
+
+	/**
+	 * Is the Node a leaf (without children)?
+	 * @return boolean
+	 */
+	public function isLeaf() : bool;
+
+	/**
+	 * Convert node (and children) to array
+	 * @param Closure|null $serializer optional callback to serialize Node value
+	 * @return array
+	 */
+	public function toArray($serializer = null) : array;
+
+	/**
+	 * Convert array structure to Node(s)
+	 * @param array $array
+	 * @param Closure|null $unserializer optional callback to unserialize Node value
+	 * @return NodeInterface
+	 */
+	public static function fromArray(array $array, $unserializer = null) : NodeInterface;
+
+	/**
+	 * Convert data structure (including children) to array
+	 * so it can be JSON-serialized
+	 * @return array
+	 */
+	public function jsonSerialize() : array;
+}
